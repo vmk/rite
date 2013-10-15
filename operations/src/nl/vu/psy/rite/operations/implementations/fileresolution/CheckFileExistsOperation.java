ba819613c9263f769f45089b5643c0d8df9ceee9
Mathijs Kattenberg <mathijsk@mathijs.local>
2013-10-15 15:46:13 +0200

    Changed CheckFileExistsOperation to check for a file on the local filesystem (local for client). The previous code to check relics was moved to CheckRelicExistsOperation.

--- operations/src/nl/vu/psy/rite/operations/implementations/fileresolution/CheckFileExistsOperation.java
+++ operations/src/nl/vu/psy/rite/operations/implementations/fileresolution/CheckFileExistsOperation.java
@@ -1,97 +1,72 @@
-// Copyright 2012 - V.M. Kattenberg - vm.kattenberg@gmail.com
-//
-// This file is part of operations
-//
-// operations is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// operations is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with operations.  If not, see <http://www.gnu.org/licenses/>.
-
 package nl.vu.psy.rite.operations.implementations.fileresolution;
 
 import java.io.File;
 
-import nl.vu.psy.rite.FileCache;
-import nl.vu.psy.rite.Rite;
 import nl.vu.psy.rite.operations.Operation;
 import nl.vu.psy.rite.operations.OperationPropertyKeys;
 import nl.vu.psy.rite.operations.implementations.GenericOperation;
 import nl.vu.psy.rite.operations.implementations.OperationUtilities;
 
-/**
- * CheckFileExistsOperation
- * 
- * @author vm.kattenberg
- */
 public class CheckFileExistsOperation extends GenericOperation {
-    private static final long serialVersionUID = -8350540320345242850L;
+	private static final long serialVersionUID = 700762448204350867L;
 
-    public enum PropertyKeys implements OperationPropertyKeys {
-        RELIC("relic", "", false);
+	public enum PropertyKeys implements OperationPropertyKeys {
+		PATH("path", "", false);
 
-        private final String key;
-        private final String defaultValue;
-        private final boolean nullable;
+		private final String key;
+		private final String defaultValue;
+		private final boolean nullable;
 
-        private PropertyKeys(String key, String defaultValue, boolean nullable) {
-            this.key = key;
-            this.defaultValue = defaultValue;
-            this.nullable = nullable;
-        }
+		private PropertyKeys(String key, String defaultValue, boolean nullable) {
+			this.key = key;
+			this.defaultValue = defaultValue;
+			this.nullable = nullable;
+		}
 
-        @Override
-        public String getDefaultValue() {
-            return defaultValue;
-        }
+		@Override
+		public String getDefaultValue() {
+			return defaultValue;
+		}
 
-        @Override
-        public String getKey() {
-            return key;
-        }
+		@Override
+		public String getKey() {
+			return key;
+		}
 
-        @Override
-        public boolean isNullable() {
-            return nullable;
-        }
-    }
+		@Override
+		public boolean isNullable() {
+			return nullable;
+		}
+	}
 
-    public CheckFileExistsOperation() {
-        super();
-        OperationUtilities.initialize(this, PropertyKeys.values());
-    }
+	public CheckFileExistsOperation() {
+		super();
+		OperationUtilities.initialize(this, PropertyKeys.values());
+	}
 
-    @Override
-    public Operation call() throws Exception {
-        try {
-            FileCache fileCache = Rite.getInstance().getFileCache();
-            File f = fileCache.getFileCache().getRelic(this.getProperty(PropertyKeys.RELIC), false);
-            if (!f.exists()) {
-                this.setProperty(GenericOperation.PropertyKeys.ERROR, "The file " + f.getAbsolutePath() + " does not exist!");
-                this.fail();
-            }
-        } catch (Exception e) {
-            this.setProperty(GenericOperation.PropertyKeys.ERROR, OperationUtilities.getStackTraceAsString(e));
-            this.fail();
-            this.complete();
-            return this;
-        }
-        this.complete();
-        return this;
-    }
+	@Override
+	public Operation call() throws Exception {
+		try {
+			File f = new File(getPath());
+			if (!f.exists()) {
+				this.setProperty(GenericOperation.PropertyKeys.ERROR, "The file " + f.getAbsolutePath() + " does not exist!");
+				this.fail();
+			}
+		} catch (Exception e) {
+			this.setProperty(GenericOperation.PropertyKeys.ERROR, OperationUtilities.getStackTraceAsString(e));
+			this.fail();
+			this.complete();
+			return this;
+		}
+		this.complete();
+		return this;
+	}
 
-    public void setRelicId(String relicId) {
-        setProperty(PropertyKeys.RELIC, relicId);
-    }
+	public void setPath(String path) {
+		setProperty(PropertyKeys.PATH, path);
+	}
 
-    public String getRelicId() {
-        return getProperty(PropertyKeys.RELIC);
-    }
+	public String getPath() {
+		return getProperty(PropertyKeys.PATH);
+	}
 }
